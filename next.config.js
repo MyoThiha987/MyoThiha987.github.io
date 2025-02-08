89d282cf90b8ad46477145aed7e0743fddfe0fc5
liam <luciferthiha881997@gmail.com>
2025-02-08 20:26:20 +0700

    commit

--- next.config.js
+++ next.config.js
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
-  basePath: '/MyoThiha987.github.io',
-  reactStrictMode: true,
+  //output: 'export',
+  //basePath: '/MyoThiha987.github.io',
+  //reactStrictMode: true,
   //assetPrefix: '/MyoThiha987.github.io/',
   images: {
     remotePatterns: [
